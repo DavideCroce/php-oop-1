35dedf283d042966e090191e18d076f768be14b2
IskahnX <davidecroce10@gmail.com>
2022-03-02 17:22:49 +0100

    styiling the site: my 3 favourite movies

--- index.php
+++ index.php
@@ -19,23 +19,30 @@ include 'movies.php';
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
     <link rel="icon" href="img/icon.png">
     <title>Movies</title>
 </head>
 
 <body>
+    <header class="d-flex align-items-center">
+        <h1 class="col-12 text-center">My 3 Favourite movies</h1>
+    </header>
     <main>
         <div class="container">
-            <ul>
+            <ul class="d-flex flex-row">
                 <?php foreach ($movies as $movie) :
                     $movie_obj = new Movie($movie['poster'], $movie['title'], $movie['year'], $movie['genre'], $movie['vote']);
                 ?>
-                    <li>
-                        <img src="img/<?= $movie_obj->poster ?>.jpg" alt="">
-                        <h3><?= $movie_obj->title ?></h3>
-                        <p><?= $movie_obj->year ?></p>
-                        <p><?= $movie_obj->genre ?></p>
-                        <p><?= $movie_obj->vote ?></p>
+                    <li class="col-3 d-flex justify-content-center">
+                        <div class="movie">
+                            <img src="img/<?= $movie_obj->poster ?>.jpg" alt="">
+                            <h3><b>Title:</b> <?= $movie_obj->title ?></h3>
+                            <p><b>Year:</b> <?= $movie_obj->year ?></p>
+                            <p><b>Genre:</b> <?= $movie_obj->genre ?></p>
+                            <p><b>Vote average:</b> <?= $movie_obj->vote_average ?></p>
+                        </div>
                     </li>
                 <? endforeach; ?>
             </ul>
